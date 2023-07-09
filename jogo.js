c0e8cf2c5aff7f733f1ca10a328c3bcb611f9f6d
Leckller <7leckler@gmail.com>
2023-07-09 12:04:17 -0300

    novas atualizações localstorage and fix esqueleto

--- jogo.js
+++ jogo.js
@@ -7,8 +7,14 @@ function savePlayer () {
 function newInterface () {
   const form = document.querySelector('form');
   const novaInterface = document.querySelector('#novaInterface');
+  const msgBemvindo = document.querySelector('#msgBemVindo');
+  const aside = document.querySelector('#aside');
+  const nomeArmazenado = localStorage.getItem("Player");
+  aside.style.display = 'flex';
   form.style.display = 'none';
   novaInterface.style.display = 'flex';
+  msgBemvindo.innerHTML = `Bem Vindo ${nomeArmazenado}`
+
 }
 
 const play = document.querySelector('#playButton')
