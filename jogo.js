d71adb5a9fd7223e92972a0351a348ac397ec834
Leckller <7leckler@gmail.com>
2023-07-09 12:39:10 -0300

    personagens

--- jogo.js
+++ jogo.js
@@ -14,14 +14,30 @@ function newInterface () {
   form.style.display = 'none';
   novaInterface.style.display = 'flex';
   msgBemvindo.innerHTML = `Bem Vindo ${nomeArmazenado}`
+}
 
+function classe () {
+  const ar = document.querySelector('#ar');
+  const fogo = document.querySelector('#fogo');
+  const agua = document.querySelector('#agua');
+  const terra  = document.querySelector('#terra');
+  const imagem = document.querySelector('#personagem');
+  if (ar.checked === true) {
+    imagem.src = "/personagens/ar_puto.png"
+  } else if (fogo.checked === true) {
+    imagem.src = "/personagens/fogo_puto.png"
+  } else if (agua.checked === true) {
+    imagem.src = "/personagens/agua_puto.png"
+  } else if (terra.checked === true) {
+    imagem.src = "/personagens/terra_puto.png"
+  }
 }
 
 const play = document.querySelector('#playButton')
-
 play.addEventListener('click', (eve) => {
     eve.preventDefault();
     savePlayer();
     newInterface();
+    classe();
   });
   
